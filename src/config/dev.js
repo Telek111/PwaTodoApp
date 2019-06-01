8afada37cffdacf83fd9b8e010d00270787fae11
Andrew Lozynskyj <lozunskyi.andrij@gmail.com>
2019-06-01 14:21:01 +0300

    remove koa server

---
+++ src/config/dev.js
@@ -0,0 +1,8 @@
+export const FirebaseConfig = {
+  apiKey: 'AIzaSyCqKO3-1aVdQDCw2vhZDeUmW9-doec2F90',
+  authDomain: 'todo-pwa-2afa9.firebaseapp.com',
+  databaseURL: 'https://todo-pwa-2afa9.firebaseio.com',
+  projectId: 'todo-pwa-2afa9',
+  storageBucket: 'todo-pwa-2afa9.appspot.com',
+  messagingSenderId: '264367764837'
+};
