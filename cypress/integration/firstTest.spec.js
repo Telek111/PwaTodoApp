902758c356db54e51fb6e5f8fa2f6f68f91695b8
Andrew Lozynskyj <lozunskyi.andrij@gmail.com>
2019-06-11 16:27:54 +0300

    write short lifecycle of app in e2e test

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -1,7 +1,22 @@
 /// <reference types="Cypress" />
 
-describe ('Window', () => {
+describe ('Short lifecycle of App', () => {
   it ('Visit the app', () => {
-    cy.visit ('/');
+    cy.visit('/');
+  });
+  it ('Create task', () => {
+    cy.visit('/');
+    cy.get('.fixed-action-btn').click();
+    cy.get('#toDoNext').type('Some Task').type('{enter}');
+  });
+  it ('Remove all tasks', () => {
+    cy.visit('/');
+    cy.get('.fixed-action-btn').click();
+    cy.get('#toDoNext').type('Some Task').type('{enter}');
+    cy.get('.complete-todo-item').click({ multiple: true });
+  });
+  it ('Todo should be zero with notification', () => {
+    cy.visit('/');
+    cy.get('h4').contains('You have no more things ToDo!');
   });
 });
